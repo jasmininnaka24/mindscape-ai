ee4482f58363e2672233aed42f8bb8fcd46de5f5
Jasmin In-naka <jasmininnaka@gmail.com>
2023-10-18 06:00:55 +0800

    9th commit

--- client/src/pages/rooms/personal/study_area/PersonalReviewerStart.jsx
+++ client/src/pages/rooms/personal/study_area/PersonalReviewerStart.jsx
@@ -4,6 +4,10 @@ import CampaignIcon from '@mui/icons-material/Campaign';
 import { useParams } from 'react-router-dom';
 import { Navbar } from '../../../../components/navbar/logged_navbar/navbar'
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+
+
 
 export const PersonalReviewerStart = () => {
 
@@ -21,9 +25,32 @@ export const PersonalReviewerStart = () => {
   const [wrongAnswerCounts, setWrongAnswerCounts] = useState(0);
   const [chanceLeft, setChanceLeft] = useState(2);
   const [points, setPoints] = useState(0);
+  const [filteredDataValue, setFilteredDataValue] = useState('')
+  
+  // for hide and unhide
+  const [hideQuestion, setHideQuestion] = useState('hidden');
+  const [unhideQuestion, setUnideQuestion] = useState('');
+  const [hideChoice, setHideChoice] = useState('hidden');
+  const [unhideChoice, setUnideChoice] = useState('');
+  const [unhiddenChoice, setUnhiddenChoice] = useState([]);
+  const [hiddenChoice, setHiddenChoice] = useState([]);
+  const [hideAllChoice, setHideAllChoice] = useState('hidden');
+  const [unhideAllChoice, setUnhideAllChoice] = useState('');
+  const [hideAll, setHideAll] = useState('hidden');
+  const [unhideAll, setUnhideAll] = useState('');
+
+
+  // wrong and correct answer
+  const [answerSubmitted, setAnswerSubmitted] = useState(false);
+  const [wrongAnswer, setWrongAnswer] = useState(false)
+  const [wrongAnswer2, setWrongAnswer2] = useState(false)
+  const [answeredWrongVal, setAnsweredWrongVal] = useState('')
+  const [answeredWrongVal2, setAnsweredWrongVal2] = useState('')
+
+  const [enabledSubmitBtn, setEnabledSubmitBtn] = useState(true);
+
 
   const speechSynthesis = window.speechSynthesis;
-
   
 
   const shuffleArray = (array) => {
@@ -41,37 +68,59 @@ export const PersonalReviewerStart = () => {
       try {
         const materialResponse = await axios.get(`http://localhost:3001/quesAns/study-material-mcq/${materialId}`);
         const fetchedQA = materialResponse.data;
-        setQA(fetchedQA);
   
-        if (fetchedQA.length > 0 && questionIndex < fetchedQA.length) {
-          const choicesResponse = await axios.get(`http://localhost:3001/quesAnsChoices/study-material/${materialId}/${fetchedQA[questionIndex].id}`);
-          setChoices(choicesResponse.data);
+        let filteredFetchedQA = [];
+        if (filteredDataValue === "Correct") {
+          filteredFetchedQA = fetchedQA.filter(item => item.response_state === 'Correct');
+        } else if (filteredDataValue === "Wrong") {
+          filteredFetchedQA = fetchedQA.filter(item => item.response_state === 'Wrong');
+        } else if (filteredDataValue === "Unattempted") {
+          filteredFetchedQA = fetchedQA.filter(item => item.response_state === 'Unattempted');
+        } else {
+          filteredFetchedQA = fetchedQA;
+        }
   
-          let shuffledArray = shuffleArray([...choicesResponse.data, { choice: fetchedQA[questionIndex].answer }])
-          setShuffledChoices(shuffledArray);
-  
-          const unattemptedCount = fetchedQA.filter(question => question.response_state === 'Unattempted').length;
-          const correctCount = fetchedQA.filter(question => question.response_state === 'Correct').length;
-          const wrongCount = fetchedQA.filter(question => question.response_state === 'Wrong').length;
-          setUnattemptedCounts(unattemptedCount);
-          setCorrectAnswerCounts(correctCount);
-          setWrongAnswerCounts(wrongCount);
+        // Ensure questionIndex does not exceed the number of filtered questions
+        const newIndex = Math.max(0, Math.min(questionIndex, filteredFetchedQA.length - 1));
+        setQuestionIndex(newIndex);
+        setQA(filteredFetchedQA);
+        console.log(filteredFetchedQA);
+        console.log(newIndex);
 
-          const questionId = fetchedQA.filter(question => question.stoppedAt === '1')[0].id;
 
-          const foundIndex = fetchedQA.findIndex(question => question.id === questionId);
-          
+        if (filteredFetchedQA[newIndex].stoppedAt === '1') {
+          const foundIndex = filteredFetchedQA.findIndex(question => question.id === filteredFetchedQA[newIndex].id);
           if (foundIndex !== -1) {
             setQuestionIndex(foundIndex);
-            console.log(foundIndex);
           } else {
-            console.error('Question not found in fetchedQA array');
+            console.error('Question not found in filteredFetchedQA array');
           }
-          
+        }
+  
+        if (filteredFetchedQA.length > 0) {
+          const currentQuestion = filteredFetchedQA[newIndex];
+          if (currentQuestion) {
+            const choicesResponse = await axios.get(`http://localhost:3001/quesAnsChoices/study-material/${materialId}/${currentQuestion.id}`);
+            const choices = choicesResponse.data;
+            const shuffledArray = shuffleArray([...choices, { choice: currentQuestion.answer }]);
+            setChoices(choices);
+            setShuffledChoices(shuffledArray);
+  
+            const unattemptedCount = filteredFetchedQA.filter(question => question.response_state === 'Unattempted').length;
+            const correctCount = filteredFetchedQA.filter(question => question.response_state === 'Correct').length;
+            const wrongCount = filteredFetchedQA.filter(question => question.response_state === 'Wrong').length;
+            setUnattemptedCounts(unattemptedCount);
+            setCorrectAnswerCounts(correctCount);
+            setWrongAnswerCounts(wrongCount);
+  
 
-
+          } else {
+            console.error("Current question is undefined.");
+            // Handle this case according to your application logic (e.g., show a message to the user, reset to a default state)
+          }
         } else {
           console.error("No questions available or invalid question index.");
+          // Handle this case according to your application logic (e.g., show a message to the user, reset to a default state)
         }
       } catch (error) {
         console.error("Error fetching data: ", error);
@@ -79,19 +128,22 @@ export const PersonalReviewerStart = () => {
     }
   
     fetchData();
-  
     // Clean up the speech synthesis on component unmount or when question changes
     return () => {
       speechSynthesis.cancel();
     };
-  }, [materialId, questionIndex, speechSynthesis]);
+  }, [materialId, filteredDataValue, questionIndex, speechSynthesis]);
+  
+  
+  
+
+  
 
   
   
 
   const handleRadioChange = (event) => {
     setSelectedChoice(event.target.value);
-    console.log(event.target.value);
   };
   
   const stateQuestion = () => {
@@ -130,32 +182,62 @@ export const PersonalReviewerStart = () => {
 
   const submitAnswer = (choice) => {
     if(selectedChoice === extractedQA[questionIndex].answer){
-      alert('Correct')
-      setSubmittedAnswer("")
-      setChanceLeft(2)
-      setPoints(points + 1)
-      currectQuestion('0', questionIndex)
-      let questionIndexVal = (questionIndex + 1) % extractedQA.length;
-      setQuestionIndex(questionIndexVal);
-      currectQuestion('1', questionIndexVal)
-      currectCounts(choice, "Correct")
-
+      setTimeout(() => {
+        unhideAllBtn()
+        setAnswerSubmitted(true)
+        setEnabledSubmitBtn(false)
+      }, 500);
+      console.log(choice);
+      setTimeout(() => {
+        setAnswerSubmitted(false)
+        setSubmittedAnswer("")
+        setChanceLeft(2)
+        setPoints(points + 1)
+        currectQuestion('0', questionIndex)
+        currectCounts(choice, "Correct")
+        hideAllBtn()
+        setEnabledSubmitBtn(true)
+        let questionIndexVal = (questionIndex + 1) % extractedQA.length;
+        setQuestionIndex(questionIndexVal);
+        currectQuestion('1', questionIndexVal)
+      }, 4000);
 
     } else {
       if(chanceLeft !== 0) {
         alert('Wrong')
         let currentChanceCount = chanceLeft - 1
         setChanceLeft(currentChanceCount)
+        if(setAnsweredWrongVal === ''){
+          setWrongAnswer(true)
+          setAnsweredWrongVal(selectedChoice)
+        } else {
+          setWrongAnswer2(true)
+          setAnsweredWrongVal2(selectedChoice)
+        }
         if(currentChanceCount === 0) {
-          setChanceLeft(2)
-          currectCounts(choice, "Wrong")
           setTimeout(() => {
+            unhideAllBtn()
+            setAnswerSubmitted(true)
+            setEnabledSubmitBtn(false)
+          }, 500);
+          
+          setTimeout(() => {
+            setAnsweredWrongVal('')
+            setAnsweredWrongVal2('')
+            setAnswerSubmitted(false)
+            setChanceLeft(2)
+            currectCounts(choice, "Wrong")
+            hideAllBtn()
             setSubmittedAnswer("")
-          }, 1000);
-          currectQuestion('0', questionIndex)
-          let questionIndexVal = (questionIndex + 1) % extractedQA.length;
-          setQuestionIndex(questionIndexVal)
-          currectQuestion('1', questionIndexVal)
+            setWrongAnswer(false)      
+            setEnabledSubmitBtn(true)      
+            currectQuestion('0', questionIndex)
+            let questionIndexVal = (questionIndex + 1) % extractedQA.length;
+            setQuestionIndex(questionIndexVal)
+            currectQuestion('1', questionIndexVal)
+          }, 3000);
+          
+
         }
       }
       
@@ -163,6 +245,57 @@ export const PersonalReviewerStart = () => {
     
   };
 
+
+  // hide button functionalities
+  const hideQuestionBtn = () => {
+    setHideQuestion('')
+    setUnideQuestion('hidden')
+  }
+
+  const unhideQuestionBtn = () => {
+    setHideQuestion('hidden')
+    setUnideQuestion('')
+  }
+
+  const hideChoiceBtn = (choice) => {
+    if (unhiddenChoice.includes(choice)) {
+      setUnhiddenChoice(unhiddenChoice.filter(item => item !== choice));
+    } else {
+      setUnhiddenChoice([...unhiddenChoice, choice]);
+    }
+  }
+
+
+  const unhideAllChoicesBtn = () => {
+    const allChoices = shuffledChoices.map(choice => choice.choice);
+    setUnhiddenChoice(allChoices);
+    setUnhideAllChoice('hidden')
+    setHideAllChoice('')
+  };
+  
+  const hideAllChoicesBtn = () => {
+    setUnhiddenChoice([]);
+    setUnhideAllChoice('')
+    setHideAllChoice('hidden')
+  };
+
+
+  const unhideAllBtn = () => {
+    unhideAllChoicesBtn()
+    setUnhideAll('hidden')
+    setHideAll('')
+
+    hideQuestionBtn();
+  };
+  
+  const hideAllBtn = () => {
+    hideAllChoicesBtn()
+    setUnhideAll('')
+    setHideAll('hidden')
+
+    unhideQuestionBtn();
+  };
+  
 
 
   return (
@@ -199,47 +332,137 @@ export const PersonalReviewerStart = () => {
         </div>
 
 
+        {/* filtering */}
+        <select
+          className="mt-12 border-thin-800 px-3 py-1 rounded-[5px] outline-none border-none mcolor-900"
+          value={filteredDataValue}
+          onChange={(e) => setFilteredDataValue(e.target.value)}
+        >
+          <option value="">All Items</option>
+          <option value="Correct">Correct Answers</option>
+          <option value="Wrong">Wrong Answers</option>
+          <option value="Unattempted">Unattempted Items</option>
+        </select>
+
+
         {/* response statements counts */}
-        <div className='mt-16 flex items-center justify-between'>
-          <span className='mcolor-800 font-medium text-lg'>Unattempted: {unattemptedCounts}</span>
-          <span className='mcolor-800 font-medium text-lg'>Correct Answer: {correctAnswerCounts}</span>
-          <span className='mcolor-800 font-medium text-lg'>Wrong Answer: {wrongAnswerCounts}</span>
+        <div className='mt-5 flex items-center justify-between'>
+          {filteredDataValue === 'Unattempted' && (
+            <span className='mcolor-800 font-medium text-lg'>Unattempted: {unattemptedCounts}</span>
+            )}
+          {filteredDataValue === 'Correct' && (
+            <span className='mcolor-800 font-medium text-lg'>Correct Answer: {correctAnswerCounts}</span>
+            )}
+          {filteredDataValue === 'Wrong' && (
+            <span className='mcolor-800 font-medium text-lg'>Wrong Answer: {wrongAnswerCounts}</span>
+            )}
+
+
         </div>
 
+        {filteredDataValue === '' && (
+          <div className='mt-5 flex items-center justify-between'>
+            <span className='mcolor-800 font-medium text-lg'>Unattempted: {unattemptedCounts}</span>
+            <span className='mcolor-800 font-medium text-lg'>Correct Answer: {correctAnswerCounts}</span>
+            <span className='mcolor-800 font-medium text-lg'>Wrong Answer: {wrongAnswerCounts}</span>
+          </div>
+        )}
 
 
-        <div className='relative mt-4 pb-8 text-center text-xl font-medium text-xl mcolor-900'>
-          <p className='mbg-300 mcolor-900 w-full rounded-[5px] py-4 mcolor-800'>Question {questionIndex + 1}</p>
-          <button className='mx-3 absolute right-5 top-4' onClick={stateQuestion}><CampaignIcon /></button>
-        </div>
 
-        <form className='grid-result gap-4'>
-          {shuffledChoices.map((choice, index) => {
-            return (
+
+
+        {extractedQA.length > 0 ? (
+          <div>
+
+            {/* question */}
+            <div className='relative mt-4 pb-8 text-center text-xl font-medium text-xl mcolor-900'>
+              <p className='mbg-300 mcolor-900 w-full rounded-[5px] py-4 mcolor-800'>{hideQuestion !== 'hidden' ? extractedQA[questionIndex].question : `Question...`}</p>
+              <button className='mx-3 absolute right-5 top-4' onClick={stateQuestion}><CampaignIcon /></button>
+              <button className={`mx-3 absolute right-12 top-4 ${hideQuestion !== 'hidden' ? 'hidden' : ''}`} onClick={hideQuestionBtn}><VisibilityIcon /></button>
+              <button className={`mx-3 absolute right-12 top-4 ${unhideQuestion !== 'hidden' ? 'hidden' : ''}`} onClick={unhideQuestionBtn}><VisibilityOffIcon /></button>
+            </div>
+
+
+            {/* functionality buttons */}
+
+            <div className='flex items-center justify-between'>
+
+              {/* hide and unhide all choices */}
+              <div>
+                <button className={`mcolor-800 mb-3 cursor-pointer text-xl ${unhideAll !== 'hidden' ? '' : 'hidden'}`} onClick={unhideAllBtn}>
+                  Unhide All <VisibilityIcon />
+                </button>
+
+                <button className={`mcolor-800 mb-3 cursor-pointer text-xl ${hideAll !== 'hidden' ? '' : 'hidden'}`} onClick={hideAllBtn}>
+                  Hide All <VisibilityOffIcon />
+                </button>
+              </div>
+
+              {/* Hide and unhide all */}
+              <div>
+                <button className={`mcolor-800 mb-3 cursor-pointer text-xl ${unhideAllChoice !== 'hidden' ? '' : 'hidden'}`} onClick={unhideAllChoicesBtn}>
+                  Unhide All Choices <VisibilityIcon />
+                </button>
+
+                <button className={`mcolor-800 mb-3 cursor-pointer text-xl ${hideAllChoice !== 'hidden' ? '' : 'hidden'}`} onClick={hideAllChoicesBtn}>
+                  Hide All Choices <VisibilityOffIcon />
+                </button>
+              </div>
+            </div>
+
+
+            {/* choices */}
+            <form className='grid-result gap-4 mcolor-800'>
+            {shuffledChoices.map((choice, index) => (
               <div
                 key={index}
-                className='flex justify-center mbg-200 px-5 py-3 text-5xl text-center mcolor-800 choice border-thin-800 rounded-[5px]'
-              >
+                className={`flex items-center justify-center px-5 py-3 text-center choice border-thin-800 rounded-[5px]  
+
+                ${answerSubmitted === true && choice.choice === extractedQA[questionIndex].answer ? 'mbg-700 mcolor-100 ' : 'mbg-200 '}
+
+                ${wrongAnswer === true && choice.choice === answeredWrongVal ? 'bg-red mcolor-100 ' : ''}
+
+                ${wrongAnswer2 === true && choice.choice === answeredWrongVal2 ? 'bg-red mcolor-100 ' : ''} 
+                
+                `}
+                >
                 <input
                   type="radio"
                   name="option"
                   value={choice.choice}
                   id={`choice${index}`} 
-                  className='custom-radio mt-1 cursor-pointer hidden'
+                  className={`custom-radio cursor-pointer`}
                   onChange={handleRadioChange}
                   checked={selectedChoice === choice.choice} 
-                  onClick={() => {choiceSpeak(choice.choice)}}
                 />
-                <label htmlFor={`choice${index}`} className='ml-1 cursor-pointer text-5xl'><CampaignIcon /></label>
+                <div className=''>
+                  <div className={`flex items-center`}>
+                    <label htmlFor={`choice${index}`} className={`mr-5 pt-1 cursor-pointer text-xl ${unhiddenChoice.includes(choice.choice) ? '' : 'hidden'}`}>
+                      {choice.choice}
+                    </label>
+
+                    <label htmlFor={`choice${index}`} className='ml-1 cursor-pointer text-5xl' onClick={() => hideChoiceBtn(choice.choice)}>
+                      {unhiddenChoice.includes(choice.choice) ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                    </label>
+                    <label htmlFor={`choice${index}`} className='ml-2 cursor-pointer text-5xl' onClick={() => {choiceSpeak(choice.choice)}}><CampaignIcon /></label>
+                  </div>
+
+                </div>
               </div>
-            );
-          })}
-        </form>
+            ))}
 
-        <div className='flex justify-center mt-8'>
-          <button className='w-1/2 py-2 px-5 mbg-700 rounded-[5px] mcolor-100 text-lg' onClick={() => submitAnswer(extractedQA[questionIndex].id)}>Submit Answer</button>
-        </div>
+            </form>
 
+            {enabledSubmitBtn === true && (
+              <div className='flex justify-center mt-8'>
+                <button className='w-1/2 py-2 px-5 mbg-700 rounded-[5px] mcolor-100 text-lg' onClick={() => submitAnswer(extractedQA[questionIndex].id)}>Submit Answer</button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <p className='text-center my-5 text-xl mcolor-500'>Nothing to show</p>
+        )}
       </div>
     </div>
   )
